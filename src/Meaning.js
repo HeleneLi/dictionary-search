65b60b69d14bd908785fb1023414808dee4de0b5
HeleneLi <89549373+HeleneLi@users.noreply.github.com>
2022-08-16 15:13:41 +0100

    fixed bug

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,20 +1,16 @@
 import React from "react";
 
 export default function Meaning(props) {
+  console.log(props.meaning);
   return (
     <div className="Meaning">
-    <h3>{props.meaning.partOfSpeech}</h3>
-    {props.meaning.definitions.map(function(definition, index) {
-      return (
-        <div key={index}>
-          <p>
-            {definition.definition}
-            <br/>
-            <em>{definition.example}</em>
-          </p>
-        </div>
-      );
-    })}
+      <h3>{props.meaning.partOfSpeech}</h3>
+      <div>
+        <p>
+          <br />
+          <em>{props.meaning.definitions[0].example}</em>
+        </p>
+      </div>
     </div>
   );
 }
