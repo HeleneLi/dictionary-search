7b62bd8ae85304a9c34eb8daa5dcc940bbcbe3c5
HeleneLi <89549373+HeleneLi@users.noreply.github.com>
2022-08-16 15:21:50 +0100

    added definitions

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,12 +5,18 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <div>
-        <p>
-          <br />
-          <em>{props.meaning.definitions[0].example}</em>
-        </p>
+      {props.meaning.definitions.map(function(definition,index)
+      {
+        return (
+          <div key={index}>
+            <p>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
       </div>
-    </div>
   );
 }
